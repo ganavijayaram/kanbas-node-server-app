9c27146ea3a69990f16835af7592d2cd5b094554
ganavijayaram <jayaram.g@northeastern.edu>
2023-11-20 20:51:00 -0600

    Modified app and .env

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-import express from 'express';
-import Hello from './hello.js';
-import Lab5 from './lab5.js';
-import cors from 'cors';
-import CourseRoutes from './courses/routes.js';
-import ModuleRoutes from './modules/routes.js';
-import AssignmentRoutes from './assignments/routes.js';
-import 'dotenv/config';
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import "dotenv/config";
 const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
